1396675421da9a3870f519e4ae9ffdd0a9bcb58d
Carlos Rodriguez <crodveg@yahoo.es>
2018-06-04 23:05:20 +0200

    Moved to separate cargo projects per set of challenges.

--- set-1-basics/src/4-detect-single-character-xor/main.rs
+++ set-1-basics/src/4-detect-single-character-xor/main.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 mod lib;
 
 fn main() {
-    let path = Path::new("./src/set-1-basics/4-detect-single-character-xor/4.txt");
+    let path = Path::new("./src/4-detect-single-character-xor/4.txt");
     match lib::read_file(&path) {
         Err(error) => println!("{}", error),
         Ok(_) => println!("Ok")
