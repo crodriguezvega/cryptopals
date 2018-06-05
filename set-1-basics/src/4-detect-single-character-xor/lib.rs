5691916a3762a2bad0862fd4d5df7b6ad79eb38c
Carlos Rodriguez <crodveg@yahoo.es>
2018-06-05 22:06:31 +0200

    Added solution to challenge 4 of set 1.

--- set-1-basics/src/4-detect-single-character-xor/lib.rs
+++ set-1-basics/src/4-detect-single-character-xor/lib.rs
@@ -2,18 +2,41 @@ use std::fs::File;
 use std::path::Path;
 use std::io::BufRead;
 use std::io::BufReader;
+use common::mappings;
+use common::xor_cipher;
 
-pub fn read_file(path: &Path) -> Result<(), &'static str> {
+pub fn decrypt(path: &Path) -> Result<String, &'static str> {
     if !path.exists() {
         Err("File not found")
     } else {
-        let file_handler = File::open(path).unwrap();
-        let file = BufReader::new(&file_handler);
-        for line in file.lines() {
-            let l = line.unwrap();
-            println!("{}", l); 
-        }
+        let lines = read_lines(path);
 
-        Ok(())
+        let mut maximum_score = 0.0;
+        let mut secret_message = String::new();
+        for line in &lines {
+            let numbers = mappings::hex_string_to_numbers(line);
+
+            (0..=255).map(|ch| xor_cipher::xor(&numbers, ch))
+                .map(|xor| mappings::chars_to_string(&xor))
+                .for_each(|msg| {
+                    let value = xor_cipher::score(&msg);
+                    if value > maximum_score {
+                        maximum_score = value;
+                        secret_message = msg;
+                    }
+                });
+        }
+        
+        Ok(secret_message)
     }
+}
+
+fn read_lines(path: &Path) -> Vec<String> {
+    let file_handler = File::open(path).unwrap();
+    let file = BufReader::new(&file_handler);
+    
+    file.lines()
+        .filter_map(|line| line.ok())
+        .filter(|line| line.is_ascii())
+        .collect()
 }
--- set-1-basics/src/4-detect-single-character-xor/main.rs
+++ set-1-basics/src/4-detect-single-character-xor/main.rs
@@ -1,11 +1,12 @@
-use std::path::Path;
+extern crate common;
 
+use std::path::Path;
 mod lib;
 
 fn main() {
     let path = Path::new("./src/4-detect-single-character-xor/4.txt");
-    match lib::read_file(&path) {
+    match lib::decrypt(path) {
         Err(error) => println!("{}", error),
-        Ok(_) => println!("Ok")
+        Ok(message) => println!("{}", message)
     };
 }
